5b28e3a8599c6900e0706e38d632899470aca00d
Victor Hernandez <victortoy29@gmail.com>
2022-06-04 10:44:33 -0500

    Carga users y index

--- views/config/users.php
+++ views/config/users.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<base href="/proyecto_ayni/">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<base href="/proyecto_ayni/">
 	<title></title>
 </head>
 <body>
